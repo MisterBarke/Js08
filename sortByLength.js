f55a32e924a28e157fcf441a4ab8eee667750f00
Ousmane <bachirmahbarke@gmail.com>
2023-02-18 22:39:55 +0100

    no changes

--- sortByLength.js
+++ sortByLength.js
@@ -1,5 +1,5 @@
 function sortByLength(){
-    const kuroko = ["kagami", "atsuchi-murasakibara", "seijuro_akashi", "mudorima", "daiki-kise", "jumpe-yuga" ];
+    const kuroko = ["kagami", "atsuchi-murasakibara", "seijuro_akashi", "mudorima", "daiki-aomine", "jumpe-yuga" ];
     kuroko.sort((a,b)=> a.length - b.length);
     console.log(kuroko);
 }
